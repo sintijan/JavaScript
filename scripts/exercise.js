f806866288c3bc7cc2ce9a4ea373264bfe05d3a2
sintijan <sintija363@inbox.lv>
2023-07-06 23:31:17 +0300

    aa

---
+++ scripts/exercise.js
@@ -0,0 +1,13 @@
+console.log("My javascript");
+
+let age = 25;
+age = 27;
+
+const id =253;
+
+console.log("vārds")
+console.log("vecums")
+console.log("irStudents")
+
+const augļi = ["Orange", "Banana", "Mango", "Kiwi"];
+console.log("augļi");
